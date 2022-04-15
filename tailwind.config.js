540f5e2fb0168cec0b3a67b7ec65c7e9e343f1df
Tarun Kundra <tarunkundra00@gmail.com>
2022-04-15 16:04:18 +0530

    screen shots

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"
@@ -12,18 +10,7 @@ module.exports = {
       gridTemplateColumns: {
         "1/5": "1fr 5fr"
       },
-      colors: {
-        red: colors.red,
-        indigo: colors.indigo,
-        blue: colors.blue,
-        green: colors.green,
-        gray: colors.gray,
-        purple: colors.purple,
-      }
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
 }
